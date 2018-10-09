5c090fa4e2323b9eb5a975388836416191f56dd5
wangdunwei <wangdunwei@wxbtc.com>
2018-10-09 09:17:50 +0800

    修改mvp

--- mvp/src/main/java/com/ayvytr/mvp/BasePresenter.java
+++ mvp/src/main/java/com/ayvytr/mvp/BasePresenter.java
@@ -1,18 +1,13 @@
 package com.ayvytr.mvp;
 
-import android.arch.lifecycle.LifecycleObserver;
-
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.annotations.NonNull;
 
 /**
  * @author ayvytr
  */
-public class BasePresenter<M extends IModel, V extends IView> implements IPresenter, LifecycleObserver {
+public class BasePresenter<M extends IModel, V extends IView> implements IPresenter{
     protected M mModel;
     protected V mView;
-
-    protected CompositeDisposable mCompositeDisposable;
 
     public BasePresenter() {
         onCreate();
@@ -24,9 +19,7 @@ public class BasePresenter<M extends IModel, V extends IView> implements IPresen
      * @param model
      * @param rootView
      */
-    public BasePresenter(M model, V rootView) {
-        Preconditions.checkNotNull(model, "%s cannot be null", IModel.class.getName());
-        Preconditions.checkNotNull(rootView, "%s cannot be null", IView.class.getName());
+    public BasePresenter(@NonNull M model, @NonNull V rootView) {
         this.mModel = model;
         this.mView = rootView;
         onCreate();
@@ -37,8 +30,7 @@ public class BasePresenter<M extends IModel, V extends IView> implements IPresen
      *
      * @param rootView
      */
-    public BasePresenter(V rootView) {
-        Preconditions.checkNotNull(rootView, "%s cannot be null", IView.class.getName());
+    public BasePresenter(@NonNull V rootView) {
         this.mView = rootView;
         onCreate();
     }
@@ -46,39 +38,14 @@ public class BasePresenter<M extends IModel, V extends IView> implements IPresen
 
     @Override
     public void onCreate() {
-        //将 LifecycleObserver 注册给 LifecycleOwner 后 @OnLifecycleEvent 才可以正常使用
-//        if (mView != null && mView instanceof LifecycleOwner) {
-//            ((LifecycleOwner) mView).getLifecycle().addObserver(this);
-//            if (mModel!= null && mModel instanceof LifecycleObserver){
-//                ((LifecycleOwner) mView).getLifecycle().addObserver((LifecycleObserver) mModel);
-//            }
-//        }
     }
 
     @Override
     public void onDestroy() {
-        dispose();
-        if (mModel != null) {
+        if(mModel != null) {
             mModel.onDestroy();
             this.mModel = null;
         }
     }
 
-    public void addDispose(Disposable disposable) {
-        if (mCompositeDisposable == null) {
-            mCompositeDisposable = new CompositeDisposable();
-        }
-        mCompositeDisposable.add(disposable);//将所有 Disposable 放入集中处理
-    }
-
-    /**
-     * 停止集合中正在执行的 RxJava 任务
-     */
-    public void dispose() {
-        if (mCompositeDisposable != null) {
-            mCompositeDisposable.clear();//保证 Activity 结束时取消所有正在执行的订阅
-            mCompositeDisposable.dispose();
-            mCompositeDisposable = null;
-        }
-    }
 }
